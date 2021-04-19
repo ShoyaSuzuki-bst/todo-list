16964e47ad4d852c6d606cf0341e5e934c440ea1
ShoyaSuzuki-bst <sh.suzuki@buysell-technologies.com>
2021-04-19 10:56:45 +0900

    rspec導入＆モデルテスト追加

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,3 +1,17 @@
 RSpec.describe Task, type: :model do
-  it "タスクとその詳細を登録すると、"
+  it 'タスクとその詳細を登録することができる。' do
+    task = Task.new(name: 'test task', detail: 'これはテスト用のタスクです。')
+    expect(task).to be_valid
+  end
+
+  it 'タスク登録時、タスク名が指定されないと登録することができない。' do
+    task = Task.new(name: nil, detail: 'バリデーションテスト')
+    task.valid?
+    expect(task.errors[:name]).to include("can't be blank")
+  end
+
+  it 'タスク詳細は空白でも登録することができる。' do
+    task = Task.new(name: 'test task', detail: nil)
+    expect(task).to be_valid
+  end
 end
