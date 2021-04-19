ed73f83c170a4efda21e3b558bb9d2403603de49
ShoyaSuzuki-bst <sh.suzuki@buysell-technologies.com>
2021-04-19 18:05:38 +0900

    FactoryBot導入＆日本語部分を英語化＆contextの追加

---
+++ spec/factories/tasks.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :task do
+    name {"test task"}
+    detail {"これはテスト用のタスクです。"}
+  end
+end
--- spec/requests/task_spec.rb
+++ spec/requests/task_spec.rb
@@ -1,73 +1,65 @@
 require 'rails_helper'
 
-RSpec.describe "Tasks", type: :request do
+RSpec.describe "Tasks controller", type: :request do
   before do
-    @task = Task.create(
-      name: 'test task',
-      detail: 'これはテスト用のタスクです。'
-    )
+    @task = FactoryBot.create(:task)
   end
 
   describe "GET /index" do
-    it 'httpステータスが200となる' do
-      get tasks_path
-      expect(response).to have_http_status(200)
+    context'http response check' do
+      it 'return status 200' do
+        get tasks_path 
+        expect(response).to have_http_status(200)
+      end
     end
   end
 
   describe "GET /show" do
-    it 'httpステータスが200となる' do
-      get task_path(@task.id)
-      expect(response).to have_http_status(200)
+    context 'http response check' do
+      it 'http status is 200' do
+        get task_path(@task.id)
+        expect(response).to have_http_status(200)
+      end
     end
   end
 
   describe "GET /new" do
-    it 'httpステータスが200となる' do
-      get new_task_path
-      expect(response).to have_http_status(200)
+    context 'http response check' do
+      it 'http status is 200' do
+        get new_task_path
+        expect(response).to have_http_status(200)
+      end
     end
   end
 
   describe "POST /create" do
     before { ActionController::Base.allow_forgery_protection = false }
     after  { ActionController::Base.allow_forgery_protection = true }
-    let(:params) do
-      {
-        task: {
-          name: '新規作成テスト',
-          detail: '新規作成できるかテスト'
-        }
-      }
-    end
-    it 'httpステータスが302となる' do
-      post '/tasks/', params: params
-      expect(response).to have_http_status(302)
+    context 'http response check' do
+      it 'http status is 302(redirect to :show)' do
+        post '/tasks/', params: {task: attributes_for(:task)}
+        expect(response).to have_http_status(302)
+      end
     end
   end
 
   describe "GET /edit" do
-    it 'httpステータスが200となる' do
-      get edit_task_path(@task.id)
-      expect(response).to have_http_status(200)
+    context 'http response check' do
+      it 'http status is 200' do
+        get edit_task_path(@task.id)
+        expect(response).to have_http_status(200)
+      end
     end
   end
 
   describe "PATCH /update" do
     before { ActionController::Base.allow_forgery_protection = false }
     after  { ActionController::Base.allow_forgery_protection = true }
-    let(:params) do
-      {
-        id: "#{@task.id}",
-        task:{
-          name: 'test task',
-          detail: '編集ができるのかテスト'
-        }
-      }
-    end
-    it 'httpステータスが302となる(showへリダイレクトされる)' do
-      patch "/tasks/#{@task.id}", params: params
-      expect(response).to have_http_status(302)
+    context 'http response check' do
+      it 'http status is 200(redirect to :show)' do
+        patch "/tasks/#{@task.id}", params: {task: attributes_for(:task)}
+        expect(response).to have_http_status(302)
+      end
     end
   end
 end
