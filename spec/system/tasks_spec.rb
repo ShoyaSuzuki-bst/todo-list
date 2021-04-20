b89c7fde4924ae9a38ec7072a72ae7609d66b301
ShoyaSuzuki-bst <sh.suzuki@buysell-technologies.com>
2021-04-20 19:06:48 +0900

    行数削減

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -4,14 +4,12 @@ RSpec.describe "Tasks", type: :system do
   before { driven_by(:rack_test) }
 
   describe '/tasks/index' do
-    FactoryBot.create(:task, name: "test1")
-    FactoryBot.create(:task, name: 'test2', created_at: Time.current + 1.days)
-    FactoryBot.create(:task, name: 'test2', created_at: Time.current + 2.days)
-    FactoryBot.create(:task, name: 'test2', created_at: Time.current + 3.days)
+    [*0..3].each do |i|
+      FactoryBot.create(:task, name: 'test2', created_at: Time.current + i.days)
+    end
     context 'access /tasks/index' do
       it 'is expected tasks list desc' do
         visit tasks_path
-
         date = all('div.task-row div.task-created_at').map{ |i| i.text.in_time_zone }
         expect(date).to eq(date.sort.reverse)
       end
