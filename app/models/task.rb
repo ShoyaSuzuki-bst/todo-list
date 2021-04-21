ad8dbe7665fd9a658c4091878924b07e6afacf2c
ShoyaSuzuki-bst <sh.suzuki@buysell-technologies.com>
2021-04-21 17:41:43 +0900

    タスクモデルに文字数制限のバリデーションを追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 25 }
+  validates :detail, length: { maximum: 660 }
 end
