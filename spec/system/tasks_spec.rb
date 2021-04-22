ead0d403369a11db6f4b4d761e7aa29aef3b1705
ShoyaSuzuki-bst <sh.suzuki@buysell-technologies.com>
2021-04-22 16:26:53 +0900

    テスト内容修正

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe "Tasks", type: :system do
     (0..3).each do |i|
       FactoryBot.create(:task, name: 'test', created_at: Time.current + i.days)
     end
+    let(:valid_datetimes) { Task.order(created_at: :desc).pluck(:created_at).map{ |i| Time.at(i.to_i / 60 * 60) } }
     context 'access /tasks/index' do
       it 'is expected tasks list desc' do
         visit tasks_path
@@ -16,9 +17,9 @@ RSpec.describe "Tasks", type: :system do
         # 表示されている内容から、全タスクの作成日時(create_at)を取得する。
         # allメソッドは上から順番に格納するはずなので、正常に動作しているならばこの時点で「作成日の新しい順」になっている。
         # string array
-        date = all('div.task-row div.task-created_at').map{ |i| i.text.in_time_zone }
+        created_datetimes = all('div.task-row div.task-created_at').map{ |i| i.text.in_time_zone }
         # 「作成日の新しい順」となっているはずのdateと、それを後からソートしたもので比較する。
-        expect(date).to eq(date.sort.reverse)
+        expect(created_datetimes).to eq(valid_datetimes)
       end
     end
   end
