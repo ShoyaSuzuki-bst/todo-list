43b43a9724e1b54616adf3ceed5e5634374a184e
ShoyaSuzuki-bst <sh.suzuki@buysell-technologies.com>
2021-04-26 13:43:03 +0900

    system/task_spec.rbを編集

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,15 +1,12 @@
 RSpec.describe Task, type: :model do
   describe 'name' do
-    let(:task) { FactoryBot.build(:task) }
-    let(:blank_name) { FactoryBot.build(:task, :blank_name) }
-    let(:overflow_name) { FactoryBot.build(:task, :overflow_name) }
-    let(:overflow_detail) { FactoryBot.build(:task, :overflow_detail) }
-
     context 'when task is valid' do
+      let(:task) { FactoryBot.build(:task) }
       it { expect(task).to be_valid }
     end
 
     context 'when name is null' do
+      let(:blank_name) { FactoryBot.build(:task, :blank_name) }
       it "is expected to return error [タスク名を入力してください]" do
         blank_name.valid?
         expect(blank_name.errors[:name]).to include("を入力してください")
@@ -17,6 +14,7 @@ RSpec.describe Task, type: :model do
     end
 
     context 'when name is overflow' do
+      let(:overflow_name) { FactoryBot.build(:task, :overflow_name) }
       it 'is expected to return error [タスク名は25文字以内で入力してください]' do
         overflow_name.valid?
         expect(overflow_name.errors[:name]).to include('は25文字以内で入力してください')
@@ -24,6 +22,7 @@ RSpec.describe Task, type: :model do
     end
 
     context 'when detail is overflow' do
+      let(:overflow_detail) { FactoryBot.build(:task, :overflow_detail) }
       it 'is expected to return error [タスク詳細sは660文字以内で入力してください' do
         overflow_detail.valid?
         expect(overflow_detail.errors[:detail]).to include('は660文字以内で入力してください')
