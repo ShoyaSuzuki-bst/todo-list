1a52eb7b74cc0861df69ec311d719a8b45455689
ShoyaSuzuki-bst <sh.suzuki@buysell-technologies.com>
2021-04-27 16:44:17 +0900

    終了期日に関するテストを追加

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :task do
     name { "test task" }
     detail { "This is test task." }
+    limited_at { Time.new(2022, 12, 31, 23, 59,59) }
 
     trait :blank_name do
       name { nil }
--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -7,11 +7,11 @@ RSpec.describe "Tasks", type: :system do
 
     # create_atが異なる4つのタスクを作成
     (0..3).each do |i|
-      FactoryBot.create(:task, name: 'test', created_at: Time.current + i.days)
+      FactoryBot.create(:task, name: 'test', limited_at: Time.current + i.days, created_at: Time.current + i.days)
     end
-    let(:valid_datetimes) { Task.order(created_at: :desc).pluck(:created_at).map{ |created_at| I18n.l(created_at, format: :short) } }
     context 'access /tasks/index' do
-      it 'is expected tasks list desc' do
+      let(:valid_datetimes) { Task.order(created_at: :desc).pluck(:created_at).map{ |created_at| I18n.l(created_at, format: :short) } }
+      it 'is expected to sort tasks descending order of created_at' do
         visit tasks_path
 
         # 表示されている内容から、全タスクの作成日時(create_at)を取得する。
@@ -20,6 +20,15 @@ RSpec.describe "Tasks", type: :system do
         created_datetimes = all('div.task-row div.task-created_at').map{ |i| i.text }
         # 「作成日の新しい順」となっているはずのdateと、DBから参照したデータをソートしたもので比較する。
         expect(created_datetimes).to eq(valid_datetimes)
+      end
+    end
+
+    context 'access /tasks/index' do
+      let(:valid_datetimes) { Task.order(:limited_at).pluck(:limited_at).map{ |created_at| I18n.l(created_at, format: :short) } }
+      it 'is expected to sort tasks descending order of limited_at' do
+        visit "/tasks/sort/limited_at"
+        display_limited_datetimes = all('div.task-row div.task-created_at').map{ |i| i.text }
+        expect(display_limited_datetimes).to eq(valid_datetimes)
       end
     end
   end
